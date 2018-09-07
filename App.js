3767e4d0e10a7b9308c5c9fb50138a4fba83ecb1
Mahesh_Nandam <nandam.uma@cowrks.com>
2018-09-08 04:50:03 +0530

    Reusable textInput component

--- App.js
+++ App.js
@@ -1,49 +1,62 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
- * @format
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar
+} from 'react-native';
+import { SafeAreaView } from 'react-navigation'; //from ˆ2 version
+import AutoTextInputHeight from './app/AutoTextInputHeight';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView forceInset={{ bottom: 'always' }} style={{
+        flex: 1,
+      }}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss()
+          }}>
+          <View style={{
+            flex: 1
+          }}>
+            <View style={{
+              flex: 1,
+              backgroundColor: '#fff',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}>
+              <Text>What's App Chat Room UI</Text>
+            </View>
+            <AutoTextInputHeight
+              backgroundColor={'#fff'}
+              borderTopColor={'#f5f5f5'}
+              placeholderText={'Aa'}
+              placeholderTextColor='#9e9e9e'
+              textColor={'#000'}
+              textInputBgColor={'#f5f5f5'}
+              editable={true}
+              multiline={true}
+              keyboardType={'default'}
+              sendButtonBgColor={'#1a75ff'}
+              sendImage={require('./images/sendIcon.png')}
+          />
+          </View>
+        </TouchableWithoutFeedback>
+      </SafeAreaView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
-import App from './app/App';
+import { AppRegistry, YellowBox } from 'react-native';
+import App from './App';
+
+
+YellowBox.ignoreWarnings([
+    'Warning: isMounted(...) is deprecated',
+    'Module RCTImageLoader requires main queue setup'
+]);
 
 AppRegistry.registerComponent('DynamicHeightTextInput', () => App);
