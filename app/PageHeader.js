f9ae1cbaf16c4f8c61249e46b215ead0c044a042
Mahesh_Nandam <nandam.uma@cowrks.com>
2018-09-08 01:52:40 +0530

    Main ChatRoom UI with Dynamic TextInput height

---
+++ app/PageHeader.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+import CommonStyle from './components/CommonStyle';
+
+export default class PageHeader extends Component{
+    
+    render(){      
+        return(
+            <View style={CommonStyle.headerStyle}>
+                <Text style={CommonStyle.headerText}>{'Home Screen'}</Text>
+            </View>
+        )
+    }
+}
