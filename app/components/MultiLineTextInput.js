faf79a49b56d554a108b798f39a6957dee15f55d
Mahesh_Nandam <nandam.uma@cowrks.com>
2018-11-13 01:54:01 +0530

    Displaying sent message with basic UI

--- app/components/MultiLineTextInput.js
+++ app/components/MultiLineTextInput.js
@@ -9,43 +9,32 @@ import {
     Text,
     View,
     Keyboard,
+    ScrollView,
+    ListView,
     StatusBar
 } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
 import WhatsAppTextInput from 'react-native-whatsapp-textinput';
+
+let messageList = [];
+const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
 export default class MultiLineTextInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
             messageText: '',
-            showMessage : false
+            sentMessage: '',
+            showMessage: false,
         }
     }
 
-    onChangedText(msgText){
+    onChangedText(msgText) {
         this.setState({ messageText: msgText })
     }
 
-    displayMessage(msgText){
-        let messageQ = [];
-        messageQ.push(this.state.messageText);
-        console.log(messageQ[0])
-        return(
-            <View>
-                {
-                    messageQ.map((item,i)=>{
-                        return(
-                            <Text key={i}> {item} </Text>
-                        )
-                    })
-                }
-            </View>
-        )
-    }
 
     validateTextInput(text) {
-        // alert(text)
         if (text.length === 0) {
             return true;
         } else {
@@ -53,27 +42,61 @@ export default class MultiLineTextInput extends Component {
         }
     }
 
+    displayMessage(msg) {
+        this.setState({ sentMessage: msg })
+        messageList.push(msg);
+        this.setState({ messageText: '' })
+    }
+
 
     render() {
         return (
             <SafeAreaView forceInset={{ bottom: 'always' }} style={{ flex: 1 }} onPress={() => Keyboard.dismiss()}>
 
-                    <View style={{
-                        flex: 1
+                <View style={{
+                    flex: 1,
+                    backgroundColor: '#000'
+                }}>
+                    <ScrollView contentContainerStyle={{
+                        flexGrow: 1,
+                        top: 0,
+                        borderRadius: 10,
+                        bottom: 60
                     }}>
-                        <View style={{
-                            flex: 1,
-                            backgroundColor: '#fff',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}>
-                            <Text>Whats App TextInput</Text>
-                        </View>
+                        <ListView
+                            enableEmptySections={true}
+                            dataSource={ds.cloneWithRows(messageList)}
+                            renderRow={(rowData) =>
+                                <View style={{
+                                    backgroundColor: 'transparent',
+                                    alignItems: 'flex-end',
+                                    // justifyContent: 'flex-end',
+                                    // position: 'absolute',
+                                    top: 10,
+                                    right: 10,
+                                }}>
+                                    <Text multiLine={true}
+                                    style={{
+                                        padding: 10,
+                                        marginTop: 10,
+                                        flexWrap: 'wrap',
+                                        textAlign: 'right',
+                                        backgroundColor: '#fff',
+                                    }}
+                                >
+                                    {rowData}
+                                </Text>
+                                </View>
+                            }
+                        />
+                    </ScrollView>
 
-                        {/* {
-                            this.displayMessage()
-                        } */}
-
+                    <View style={{
+                        position: 'absolute',
+                        bottom: 0,
+                        left: 0,
+                        right: 0
+                    }}>
                         <WhatsAppTextInput
                             backgroundColor={'#fff'}
                             borderTopColor={'#f5f5f5'}
@@ -88,12 +111,13 @@ export default class MultiLineTextInput extends Component {
                             sendButtonImage={require('../../images/sendIcon.png')}
                             sendButtonDisableColor={'#f5f5f0'}
                             sendButtonEnableColor={'#002080'}
-                            onChange={(msg)=>this.onChangedText(msg)}
+                            onChange={(msg) => this.onChangedText(msg)}
                             onPressButton={() => this.displayMessage(this.state.messageText)}
                             validateButton={() => this.validateTextInput(this.state.messageText)}
                             messageText={this.state.messageText}
                         />
                     </View>
+                </View>
             </SafeAreaView>
         );
     }
